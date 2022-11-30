58ee9df5b8cff92e55fc4b9ef4e957295dee10a9
kamkara <lnclass.kamkara@gmail.com>
2022-11-30 16:53:44 +0000

    Add gender

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_29_194320) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_30_164754) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
@@ -79,7 +79,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_29_194320) do
     t.string "employer"
     t.string "source"
     t.string "avatar"
-    t.boolean "gender"
+    t.string "gender"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
