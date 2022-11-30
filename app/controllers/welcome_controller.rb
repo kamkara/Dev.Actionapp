cd243591c607311688742cf623893ad156ff9fe0
kamkara <lnclass.kamkara@gmail.com>
2022-11-30 16:11:42 +0000

    app: Add fix footer linnks

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,6 @@
 class WelcomeController < ApplicationController
   def index
-    
+    @HomeMedias = Medium.all
     redirect_to dashboard_path if user_signed_in?
     @HomeArticles = Article.where("status= ?","La une").order("created_at desc").take(7)
     @HomeProjets = Projet.where("status= ?","La une").order("created_at desc").take(2) #Projet a la lune
